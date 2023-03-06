9c672fe98f2416f07df5c91bda59cb31d02174ca
eduardoabporto <eduardo.porto@innovasys.com.br>
2023-03-06 15:51:35 -0300

    API Cliente

---
+++ src/main/java/mierim/model/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package mierim.model.repository;
+
+import mierim.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
---
+++ src/main/java/mierim/rest/ClienteController.java
@@ -0,0 +1,69 @@
+package mierim.rest;
+
+import lombok.RequiredArgsConstructor;
+import mierim.model.entity.Cliente;
+import mierim.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/cliente")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    @Autowired
+    private final ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> listarTodos(){
+        return clienteRepository
+                .findAll();
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+        return clienteRepository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não Encontrado"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody @Valid Cliente cliente){
+        return clienteRepository
+                .save(cliente);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarDadosCliente(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+        clienteRepository
+                .findById(id)
+                .map(cliente -> {
+                    clienteAtualizado
+                            .setId(cliente.getId());
+                    return clienteRepository
+                            .save(clienteAtualizado);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não Encontrada"));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Integer id){
+        clienteRepository
+                .findById(id)
+                .map(cliente -> {
+                    clienteRepository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não Encontrada"));
+    }
+
+}
