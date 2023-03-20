ca2b708f9f15bad636c7a1e13d5f6dedc3719c2a
vjsjur <vagner.santos@mierim.com.br>
2023-03-20 18:32:03 -0300

    Inicio do desenvolvimento da entidade de Processos

--- src/main/java/mierim/model/entity/faturamento/Profissional.java
+++ src/main/java/mierim/model/entity/faturamento/Profissional.java
@@ -107,20 +107,23 @@ public class Profissional implements Serializable {
     private Cargo cargo;
 
 
-    @NotNull(message = "Preencha o Grupo Econômico!")
+
     @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "id_tenant")
     private Sis_CompanyGroup sis_company_group;
 
-    @NotNull(message = "Preencha a Empresa!")
+
     @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "id_tenant_company")
     private Sis_CompanyEmpresa sis_empresa;
 
-    @NotNull(message = "Preencha a Filial!")
+
     @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "id_tenant_filial")
     private Sis_CompanyFilial sis_filial;
+
+
+
 
     @Override
     public int hashCode() {
