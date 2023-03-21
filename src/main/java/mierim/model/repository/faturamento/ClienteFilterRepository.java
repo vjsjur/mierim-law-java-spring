2c2dc457e848f2cdb7e2c6fe959ddb211b2ebab5
eduardoabporto <eduardo.porto@innovasys.com.br>
2023-03-21 08:18:06 -0300

    Criação Repository Faturamento

--- src/main/java/mierim/model/repository/faturamento/ClienteFilterRepository.java
+++
@@ -1,7 +0,0 @@
-package mierim.model.repository.faturamento;
-
-import mierim.model.entity.faturamento.ClienteFilter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteFilterRepository extends JpaRepository<ClienteFilter, Integer> {
-}
