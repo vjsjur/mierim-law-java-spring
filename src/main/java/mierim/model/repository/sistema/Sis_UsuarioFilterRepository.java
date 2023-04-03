7800eba47ec5475f22369ea89ecfddc880ce59b5
vjsjur <vagner.santos@mierim.com.br>
2023-04-03 18:03:46 -0300

    Criação dos repository de sistema

--- src/main/java/mierim/model/repository/sistema/Sis_UsuarioFilterRepository.java
+++
@@ -1,7 +0,0 @@
-package mierim.model.repository.sistema;
-
-import mierim.model.entity.sistema.Sis_UsuarioFilter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Sis_UsuarioFilterRepository extends JpaRepository<Sis_UsuarioFilter, Long> {
-}
