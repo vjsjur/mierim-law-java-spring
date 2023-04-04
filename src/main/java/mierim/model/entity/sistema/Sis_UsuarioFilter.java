21e1b5679bb8c369517128539d070ec1261c323a
eduardoabporto <eduardo.porto@innovasys.com.br>
2023-04-04 08:33:59 -0300

    Ajuste no Repository e Entity da Sis_UsuarioFilter, pois precisou incluir o campo ID e a anotação Entity.

--- src/main/java/mierim/model/entity/sistema/Sis_UsuarioFilter.java
+++ src/main/java/mierim/model/entity/sistema/Sis_UsuarioFilter.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
@@ -16,10 +13,13 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Sis_UsuarioFilter implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String rg;
 	private String cpf;
 
--- src/main/java/mierim/model/repository/sistema/Sis_UsuarioFilterRepository.java
+++ src/main/java/mierim/model/repository/sistema/Sis_UsuarioFilterRepository.java
@@ -3,5 +3,5 @@ package mierim.model.repository.sistema;
 import mierim.model.entity.sistema.Sis_UsuarioFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Sis_UsuarioFilterRepository extends JpaRepository<Sis_UsuarioFilter, Long> {
+public interface Sis_UsuarioFilterRepository extends JpaRepository<Sis_UsuarioFilter, Integer> {
 }
